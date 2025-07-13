e4a868b4e6255eeec2dea70c383d20865fe343f2
Guilherme França Fernandes <guilfranca27@gmail.com>
2025-07-13 20:29:33 -0300

    Aula 4 - Finalizada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { FormularioContatoComponent } from './paginas/formulario-contato/formulario-contato.component';
+import { ListaContatosComponent } from './paginas/lista-contatos/lista-contatos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'formulario',
+        component: FormularioContatoComponent
+    },
+    {
+        path: 'lista-contatos',
+        component: ListaContatosComponent
+    },
+    {
+        path: '',
+        redirectTo: '/lista-contatos',
+        // Quando o path está vazio é necessário ter o pathMatch
+        // Roteador analisa o inicio da URL e qualquer um que tenha o host 4200 será utilizado
+        // pathMatch: 'prefix'
+        // Já o full analisa a URL por inteiro, que é o que precisamos
+        pathMatch: 'full'
+    }
+];
--- src/app/paginas/lista-contatos/lista-contatos.component.ts
+++ src/app/paginas/lista-contatos/lista-contatos.component.ts
@@ -1,7 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ContainerComponent } from '../../componentes/container/container.component';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
 import { CabecalhoComponent } from '../../componentes/cabecalho/cabecalho.component';
+import { ContainerComponent } from '../../componentes/container/container.component';
+import { SeparadorComponent } from '../../componentes/separador/separador.component';
+import { ContatoComponent } from '../../componentes/contato/contato.component';
 
 interface Contato {
   id: number
@@ -10,22 +15,18 @@ interface Contato {
 }
 
 import agenda from '../../agenda.json';
-import { FormularioContatoComponent } from '../../paginas/formulario-contato/formulario-contato.component';
-import { SeparadorComponent } from '../../componentes/separador/separador.component';
-import { ContatoComponent } from '../../componentes/contato/contato.component';
-import { FormsModule } from '@angular/forms';
-
 
 @Component({
   selector: 'app-lista-contatos',
   standalone: true,
   imports: [
+    CommonModule,
     ContainerComponent, 
     CabecalhoComponent, 
     SeparadorComponent,
     ContatoComponent,
     FormsModule,
-    FormularioContatoComponent
+    RouterLink
   ],
   templateUrl: './lista-contatos.component.html',
   styleUrl: './lista-contatos.component.css'
